b3343e7cf39c364f43aa95f2ce2be49ec8093cf9
hurshid3170 <hurshid3170@gmail.com>
2025-10-24 18:12:55 +0000

    first commit

---
+++ resources.tf
@@ -0,0 +1,21 @@
+resource "aws_iam_policy" "existing_policy" {
+  name   = "cmtr-xv69vdlr-iam-policy"
+  
+  # 1. ADD THIS LINE to match the imported description
+  description = "Custom role with limited permissions"
+
+  # 2. UPDATE THE POLICY JSON to match the imported policy
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:*",  # This must match the state
+          "s3:*"    # This must match the state
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
